4dc5a97bb77a062c6c3208f244d0b4245f845416
eddy <edv2@williams.edu>
2020-01-05 16:06:43 -0500

    first version of the login page is some what functional, I need to figure out how to position elements on a page more precisely

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Container, Col, Row} from 'reactstrap' 
+import {Container, Col, Row, Form, FormGroup, Input, Label, Button} from 'reactstrap' 
 
 //style dependencies
 import './styles/login.css'
@@ -20,16 +20,33 @@ class Login extends Component {
         <div className = "login-background" style = {backgroundStyle}>
             <Container>
                <Row>
-                   <Col sm ="12" lg={{ size: 8, offset: 2}}>
-                        <Row>
-                            <Col>
+                   <Col className = "login-container" sm ="12" lg={{ size: 8, offset: 2}}>
+                        <Row className = "login-row">
+                            <Col className = "login-panel-left">
                                 <Row>
-                                    <img width = "100%"src = {logo}/>
+                                    <img alt = "Tunnel City Logo" className = "login-logo" src = {logo}/>
                                 </Row>
-                                Tunnel City Scheduler App
+                                <p className = "login-app-name">
+                                    Tunnel City Scheduler App
+                                </p>
                             </Col>
-                            <Col>
-                                Hello world on the right
+                            
+                            
+                            <Col className  = "login-panel-right">
+                                <h1 className = "login-admin-label">Scheduler Login</h1>
+
+                                <Form className = "login-form-container">
+                                    <FormGroup >
+                                        <Label className = "login-form-label"for="exampleEmail">Email</Label>
+                                        <Input type="email" name="email" id="exampleEmail" placeholder="user@email.com" />
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Label className = "login-form-label" for="examplePassword">Password</Label>
+                                        <Input type="password" name="password" id="examplePassword" placeholder="☕️☕️☕️☕️☕️☕️" />
+                                    </FormGroup>
+                                    <Button className = "login-btn" size="md" block>Login</Button>
+                                    <p className = "login-form-label forgot-password"> Forgot login information?</p>
+                                </Form>
                             </Col>
                         </Row>
                    </Col>
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+// import 'mdbreact/dist/css/mdb.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
